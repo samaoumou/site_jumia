e69f4e7426cd20c5e8c6f7473d7351b19f6a183e
issa <i.ndiaye@isepdiamniadio.edu.sn>
2022-09-05 09:41:43 +0000

    termine

--- jumia.js
+++ jumia.js
@@ -69,11 +69,9 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " w3-white";
 } */
-
 function currentDiv(n) {
   showDivs(slideIndex = n);
 }
-
 var id = null;
 var a=1;
 function myMove(n) {
